493acf44f6d1634c8b59b6ce307f5de630ddda1c
aashutosh <aashutoshpoudel96@gmail.com>
2022-12-16 18:47:11 +0545

    assignment

--- Week 2/Assignments/starter_code.py
+++ Week 2/Assignments/starter_code.py
@@ -33,9 +33,31 @@ def rank(pwd: str) -> str:
     Returns: rank -> rank of password; POOR / MODERATE / STRONG
     '''
     ## Start code here
+    rank = ""
     
+    # Request password from user
+    pwd = input("Enter password: ")
+    
+    # Check for POOR password
+    if any(char.islower() for char in pwd) and any(char.isdigit() for char in pwd) and len(pwd) < 8:
+        rank = "POOR"
+        return rank
+    
+    # Check for MODERATE password
+    if any(char.islower() for char in pwd) and any(char.isdigit() for char in pwd) and any(char.isupper() for char in pwd) and 8 <= len(pwd) <= 10:
+        rank = "MODERATE"
+        return rank
+    
+    # Check for STRONG password
+    if any(char.islower() for char in pwd) and any(char.isdigit() for char in pwd) and any(char.isupper() for char in pwd) and any(char in string.punctuation for char in pwd) and len(pwd) > 10:
+        rank = "STRONG"
+        return rank
     ## End code here
+    # i use the any() function to check if the password contains at least one lowercase letter, one digit, one uppercase letter,
+    #and one symbol from the string.punctuation string. It also uses the islower(), isdigit(), and isupper() string methods 
+    #to check the type of each character in the password.
     return rank
+   
 
 def option1():
     '''
@@ -50,14 +72,35 @@ def option1():
     # 5. Close necessary files and print to terminal.
     
     ## START CODE HERE
+    # Ask user for input file
+    filename = input("Enter file name containing username and password: ")
+
+    # Open input file and output file
+    with open(filename, "r") as infile, open("Users-Pwds-Chked.txt", "w") as outfile:
+    # Read and store lines from input file
+            usrpwds = infile.readlines()
+    
+    # Iterate over lines
+    for line in usrpwds:
+        # Split line into username and password
+        username, password = line.strip().split(",")
+        
+        # Rank password
+        pwd_rank = rank(password)
+        
+        # Write to output file
+        outfile.write(f"{username},{password},{pwd_rank}\n")
+
+        # Print to terminal
+    print("Username and password information written to Users-Pwds-Chked.txt")
 
     ## END CODE HERE
 
-    print('#'*80)
+print('#'*80)
     # [INFO] Be sure to change userpwds with the name of variable that you give to the list of passwords
-    print('[INFO] '+'Number of passwords checked:',str(len(usrpwds))) 
-    print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
-    print('#'*80)
+print('[INFO] '+'Number of passwords checked:',str(len(usrpwds))) 
+print('[INFO] '+'The given rankings can be found in Users-Pwds-Chked.txt')
+print('#'*80)
 
 def option2():
     '''
@@ -90,6 +133,14 @@ def option2():
         # While the required ranking is not met continue joining new Ualphabet, Lalphabet, chars and digits.
         
         ## START CODE HERE
+    while not (any(char.islower() for char in pwd) and any(char.isdigit() for char in pwd) and any(char.isupper() for char in pwd) and any(char in string.punctuation for char in pwd) and len(pwd) > 10):
+        pwd = ''
+        pwd += random.choice(Ualphabets)
+        pwd += random.choice(Lalphabets)
+        pwd += random.choice(chars)
+        pwd += random.choice(digits)
+        for i in range(random.randint(4, 8)):
+            pwd += random.choice(Ualphabets + Lalphabets + chars + digits)
 
         ## END CODE HERE
         return pwd
@@ -97,29 +148,59 @@ def option2():
     # Ask for username and check 20 character limits
 
     ## START CODE HERE
+    username = input("Enter username (max 20 characters): ")
+    while len(username) > 20:
+        print("Username must be 20 characters or less.")
+    username = input("Enter username (max 20 characters): ")
 
     ## END CODE HERE
 
     # Generate the password using generate() and follow the steps as guided in the function header. 
 
     ## START CODE HERE
+    password = generate()
+save = input("Save password for user? (Y/N): ")
+
+if save == "Y":
+    # Open Users-Pwds.txt file and append username and password
+    with open("Users-Pwds.txt", "a") as f:
+        f.write(f"{username},{password}\n")
+    print("Password saved for user.")
+else:
+    # Ask if user wants to generate different password
+    generate_different = input("Generate different password for user? (Y/N): ")
+    if generate_different == "Y":
+        password = generate()
+        print("New password generated:", password)
+    else:
+        print("Password not saved for user.")
 
     ## END CODE HERE
 
 def main():
 
     print('Welcome to my password ranking program')
-    while True:
-        print('-'*40)
-        print('Please select one of 3 options')
-        print('option1. Rank password from an existing file \t option2. Generate a strong password \noption3. exit the program')
-        inp = input("Enter your option here:")
-        
-        # try converting the inp to integer form and then check condition if input was either option1, 2, 3 or something else. 
-        # exit the loop by using the break command if the user selects 3 other wise use option1() and option 2() function 
-
-        ## START CODE HERE
-
+while True:
+    print('-'*40)
+    print('Please select one of 3 options')
+    print('option1. Rank password from an existing file \t option2. Generate a strong password \noption3. exit the program')
+    inp = input("Enter your option here:")
+    
+    # try converting the inp to integer form and then check condition if input was either option1, 2, 3 or something else. 
+    # exit the loop by using the break command if the user selects 3 other wise use option1() and option 2() function 
+     ## START CODE HERE
+    try:
+        inp = int(inp)
+        if inp == 1:
+            option1()
+        elif inp == 2:
+            option2()
+        elif inp == 3:
+            break
+        else:
+            print("Invalid option. Please try again.")
+    except ValueError:
+        print("Invalid input. Please enter a valid option.")
         ## END CODE HERE
 
 
